d453d882f54b166c8e245e680d332ea9b8e451ad
lhthang1998 <lhthang.1998@gmail.com>
2020-07-04 17:30:22 +0700

    fix cors

--- app/init.go
+++ app/init.go
@@ -14,17 +14,18 @@ type Routes struct {
 
 func (app Routes) StartGin() {
 	r := gin.Default()
+
+	r.Use(gin.Logger())
+	r.Use(middlewares.NewRecovery())
+	r.Use(middlewares.NewCors([]string{"*"}))
+	r.GET("swagger/*any",middlewares.NewSwagger())
+	
 	publicRoute := r.Group("/api/v1")
 	resource, err := db.InitResource()
 	if err != nil {
 		logrus.Error(err)
 	}
 	defer resource.Close()
-
-	r.Use(gin.Logger())
-	r.Use(middlewares.NewRecovery())
-	r.Use(middlewares.NewCors([]string{"*"}))
-	r.GET("swagger/*any",middlewares.NewSwagger())
 
 	r.Static("/template/css", "./template/css")
 	r.Static("/template/images", "./template/images")
